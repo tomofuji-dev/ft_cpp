c534bb468d5b43f92a63f95234d743578c4b2288
tfuji <tomofuji.dev@gmail.com>
2022-12-21 20:58:30 +0900

    add function to class

---
+++ cpp00/ex01/include/Contract.hpp
@@ -0,0 +1,14 @@
+#ifndef CONTRACT_H
+# define CONTRACT_H
+
+class Contract {
+private:
+	/* data */
+public:
+	Contract(void);
+	~Contract(void);
+
+	void	bar(void);
+};
+
+#endif
---
+++ cpp00/ex01/src/Contract.cpp
@@ -0,0 +1,11 @@
+#include "Contract.hpp"
+
+Contract::Contract(/* args */) {
+};
+
+Contract::~Contract(void) {
+};
+
+void	Contract::bar(void) {
+	
+}
