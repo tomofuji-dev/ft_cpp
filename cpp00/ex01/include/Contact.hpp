2b44efac4103643985bd5962c951215a6cd8e1eb
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2023-01-04 16:12:59 +0900

    add contact

---
+++ cpp00/ex01/include/Contact.hpp
@@ -0,0 +1,32 @@
+#ifndef CONTACT_H
+# define CONTACT_H
+
+#include <iostream>
+
+class Contact {
+private:
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
+
+public:
+	Contact();
+	Contact(std::string first_name, std::string last_name, \
+			std::string nickname, std::string phone_number, \
+			std::string darkest_secret);
+	Contact(Contact const & src);
+	~Contact();
+
+	Contact &	operator=(Contact const &rhs);
+
+	std::string	get_first_name() const;
+	std::string	get_last_name() const;
+	std::string	get_nickname() const;
+	std::string	get_phone_number() const;
+	std::string	get_darkest_secret() const;
+	void		print_all_fields() const;
+};
+
+#endif
---
+++ cpp00/ex01/src/Contact.cpp
@@ -0,0 +1,65 @@
+#include "Contact.hpp"
+
+Contact::Contact(): first_name(std::string()), \
+						last_name(std::string()), \
+						nickname(std::string()), \
+						phone_number(std::string()), \
+						darkest_secret(std::string()){
+}
+
+Contact::Contact(std::string first_name, std::string last_name, \
+				 std::string nickname, std::string phone_number, \
+				 std::string darkest_secret):
+						first_name(first_name), \
+						last_name(last_name), \
+						nickname(nickname), \
+						phone_number(phone_number), \
+						darkest_secret(darkest_secret){
+}
+
+Contact::Contact(Contact const & src): first_name(src.get_first_name()), \
+										last_name(src.get_last_name()), \
+										nickname(src.get_nickname()), \
+										phone_number(src.get_phone_number()), \
+										darkest_secret(src.get_darkest_secret()) {
+}
+
+Contact::~Contact() {
+}
+
+Contact &	Contact::operator=(Contact const &rhs) {
+	this->first_name = rhs.get_first_name();
+	this->last_name = rhs.get_last_name();
+	this->nickname = rhs.get_nickname();
+	this->phone_number = rhs.get_phone_number();
+	this->darkest_secret = rhs.get_darkest_secret();
+	return (*this);
+}
+
+std::string	Contact::get_first_name() const {
+	return (this->first_name);
+}
+
+std::string	Contact::get_last_name() const {
+	return (this->last_name);
+}
+
+std::string	Contact::get_nickname() const {
+	return (this->nickname);
+}
+
+std::string	Contact::get_phone_number() const {
+	return (this->phone_number);
+}
+
+std::string	Contact::get_darkest_secret() const {
+	return (this->darkest_secret);
+}
+
+void Contact::print_all_fields() const {
+	std::cout << "    first name :" << this->first_name << std::endl;
+	std::cout << "     last name :" << this->last_name << std::endl;
+	std::cout << "      nickname :" << this->nickname << std::endl;
+	std::cout << "  phone number :" << this->phone_number << std::endl;
+	std::cout << "darkest secret :" << this->darkest_secret << std::endl;
+}
