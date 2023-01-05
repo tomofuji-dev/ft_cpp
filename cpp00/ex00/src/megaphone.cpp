869dbaafb757ed891354cf04c4879f55ae55ef76
tfuji <tomofuji.dev@gmail.com>
2023-01-05 16:46:43 +0900

    modify megaphone

--- cpp00/ex00/src/megaphone.cpp
+++ cpp00/ex00/src/megaphone.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
 
-bool	ft_islower(int c) {
-	return ('a' <= c && c <= 'z');
-}
-
-char	ft_get_upper(char c) {
-	if (ft_islower(c)) {
-		return (c + 'A' - 'a');
-	}
-	else
-		return (c);
-}
+#define USAGE	"* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+#define SUCCESS	0
+#define FAILURE	1
 
 int	main(int argc, char *argv[]) {
-	const char	usage[] = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-
 	if (argc == 1) {
-		std::cout << usage << std::endl;
-		return (0);
+		std::cout << USAGE << std::endl;
+		return (FAILURE);
 	}
 	for (int i = 1; i < argc; i++) {
-		for (int j = 0; argv[i][j] != '\0'; j++) {
-			std::cout << ft_get_upper(argv[i][j]);
+		std::string	s = argv[i];
+		for (size_t j = 0; j < s.size(); j++) {
+			s[j] = std::toupper(s[j]);
 		}
+		std::cout << s;
 	}
 	std::cout << std::endl;
-	return (0);
+	return (SUCCESS);
 }
