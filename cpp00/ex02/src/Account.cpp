e1ac79d4f0ec4a6c9a7fc3d501f63fe7c65a9c71
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2023-02-09 11:41:15 +0900

    add timestamp

--- cpp00/ex02/src/Account.cpp
+++ cpp00/ex02/src/Account.cpp
@@ -6,17 +6,25 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 11:20:23 by tfujiwar          #+#    #+#             */
-/*   Updated: 2023/02/07 11:56:12 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2023/02/07 14:00:25 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 
 int	Account::_nbAccounts = 0;
 
 Account::Account( int initial_deposit ) : _amount(initial_deposit) {
 	this->_accountIndex = Account::_nbAccounts;
 	Account::_nbAccounts++;
-	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+};
+
+void	Account::_displayTimestamp() {
+	std::time_t now = std::time(nullptr);
+	std::cout << "[" << std::put_time(std::localtime(&now), "%Y%m%d_%H%M%S") << "]";
 };
