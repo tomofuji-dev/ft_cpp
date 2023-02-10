4d12cfe6a6a805b3d1934f8a6934dc99f0aceb10
tfuji <tomofuji.dev@gmail.com>
2023-02-10 11:28:05 +0900

    init by template

---
+++ cpp01/ex05/include/.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   .hpp                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: t.fuji <t.fuji@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 10:41:56 by t.fuji            #+#    #+#             */
+/*   Updated: 2023/02/10 11:25:44 by t.fuji           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class className {
+
+public:
+
+	className();
+	className(className const & src);
+	~className();
+	className&	operator=(className const &rhs);
+
+private:
+
+};
