ceeffa7b333a0c46d17ea60cebb6f898bf0bfc0d
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2023-02-14 12:00:33 +0900

    delete header

---
+++ cpp03/ex01/src/main.cpp
@@ -0,0 +1,19 @@
+#include "ClapTrap.hpp"
+
+int main() {
+	ClapTrap a;
+	ClapTrap b("hoge");
+	ClapTrap c(b);
+	a = b;
+	a.attack("fuga");
+
+	for (int i = 1; i < 4; i++) {
+		a.takeDamage(i * 4);
+	}
+
+	a.attack("fuga");
+
+	for (int i = 1; i < 4; i++) {
+		a.beRepaired(i * 4);
+	}
+}
