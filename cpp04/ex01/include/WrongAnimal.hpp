877c33d1ea8af89a301c17888ad5088f9d63556f
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2023-02-17 19:46:31 +0900

    add previous exercise

---
+++ cpp04/ex01/include/WrongAnimal.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+
+#define R		"\033[31m"
+#define G		"\033[32m"
+#define Y		"\033[33m"
+#define B		"\033[34m"
+#define E		"\033[00m"
+
+class WrongAnimal {
+
+public:
+
+	WrongAnimal();
+	WrongAnimal(const std::string type);
+	WrongAnimal(const WrongAnimal &src);
+	~WrongAnimal();
+	WrongAnimal&	operator=(const WrongAnimal &rhs);
+
+	void 				makeSound() const;
+	const std::string&	getType() const;
+
+protected:
+
+	std::string type_;
+
+};
