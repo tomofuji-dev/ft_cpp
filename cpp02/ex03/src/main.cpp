4f376beb0b07741769919925e137166ef44c20a8
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2023-02-27 16:26:50 +0900

    add test bsp

--- cpp02/ex03/src/main.cpp
+++ cpp02/ex03/src/main.cpp
@@ -7,7 +7,21 @@ int main( void ) {
 	Point O(0, 0);
 	Point X(1, 0);
 	Point Y(0, 1);
-	Point P(0.5, 0);
+	Point P1(0, 0);
+	Point P2(1, 0);
+	Point P3(0, 1);
+	Point P4(0.5, 0.5);
+	Point P5(0, 0.5);
+	Point P6(0.5, 0);
+	Point P7(0.5, 0.5);
+	Point P8(0.25, 0.25);
 
-	std::cout << bsp(O, X, Y, P) << std::endl;
+	std::cout << bsp(O, X, Y, P1) << std::endl;
+	std::cout << bsp(O, X, Y, P2) << std::endl;
+	std::cout << bsp(O, X, Y, P3) << std::endl;
+	std::cout << bsp(O, X, Y, P4) << std::endl;
+	std::cout << bsp(O, X, Y, P5) << std::endl;
+	std::cout << bsp(O, X, Y, P6) << std::endl;
+	std::cout << bsp(O, X, Y, P7) << std::endl;
+	std::cout << bsp(O, X, Y, P8) << std::endl;
 }
