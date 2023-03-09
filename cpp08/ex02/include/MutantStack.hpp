d793a21fed5e619146e46aa351c5f850d1aa956f
tfuji <tomofuji.dev@gmail.com>
2023-03-09 17:28:58 +0900

    add const reverse iterator to cpp08

--- cpp08/ex02/include/MutantStack.hpp
+++ cpp08/ex02/include/MutantStack.hpp
@@ -7,7 +7,10 @@ class MutantStack : public std::stack<T, Container> {
 
 public:
 
-	typedef typename Container::iterator iterator;
+	typedef typename Container::iterator				iterator;
+	typedef typename Container::const_iterator			const_iterator;
+	typedef typename Container::reverse_iterator		reverse_iterator;
+	typedef typename Container::const_reverse_iterator	const_reverse_iterator;
 
 	MutantStack() : std::stack<T, Container>() {};
 	MutantStack(const MutantStack& src) : std::stack<T, Container>(src) {};
@@ -19,5 +22,29 @@ public:
 
 	iterator end(void) {
 		return this->c.end();
+	}
+
+	const_iterator begin(void) const {
+		return this->c.cbegin();
+	}
+
+	const_iterator end(void) const {
+		return this->c.cend();
+	}
+
+	reverse_iterator rbegin(void) {
+		return this->c.rbegin();
+	}
+
+	reverse_iterator rend(void) {
+		return this->c.rend();
+	}
+
+	const_reverse_iterator rbegin(void) const {
+		return this->c.crbegin();
+	}
+
+	const_reverse_iterator rend(void) const {
+		return this->c.crend();
 	}
 };
