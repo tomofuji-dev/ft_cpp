4efce9ca5d1afefdd778fd657a827914ef17d997
tfuji <tomofuji.dev@gmail.com>
2023-03-11 15:20:32 +0900

    add extra test to cpp08/ex02

--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -3,26 +3,52 @@
 
 int main()
 {
-MutantStack<int> mstack;
-mstack.push(5);
-mstack.push(17);
-std::cout << mstack.top() << std::endl;
-mstack.pop();
-std::cout << mstack.size() << std::endl;
-mstack.push(3);
-mstack.push(5);
-mstack.push(737);
-//[...]
-mstack.push(0);
-MutantStack<int>::iterator it = mstack.begin();
-MutantStack<int>::iterator ite = mstack.end();
-++it;
---it;
-while (it != ite)
-{
-std::cout << *it << std::endl;
-++it;
-}
-std::stack<int> s(mstack);
-return 0;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+	const MutantStack<int>cmstack = mstack;
+	MutantStack<int>::const_iterator cit = cmstack.begin();
+	MutantStack<int>::const_iterator cite = cmstack.end();
+	MutantStack<int>::const_reverse_iterator crit = cmstack.rbegin();
+	MutantStack<int>::const_reverse_iterator crite = cmstack.rend();
+	++it;
+	--it;
+	std::cout << "iterator" << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::cout << "const_iterator" << std::endl;
+	while (cit != cite)
+	{
+		std::cout << *cit << std::endl;
+		++cit;
+	}
+	std::cout << "reverse_iterator" << std::endl;
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+	std::cout << "const_reverse_iterator" << std::endl;
+	while (crit != crite)
+	{
+		std::cout << *crit << std::endl;
+		++crit;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
