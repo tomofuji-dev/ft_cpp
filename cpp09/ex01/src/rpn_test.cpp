5014de965fa00111edb88178acc0341dd9280bac
tfuji <tomofuji.dev@gmail.com>
2023-04-05 19:23:04 +0900

    add ex01

--- cpp09/ex01/src/rpn_test.cpp
+++ cpp09/ex01/src/rpn_test.cpp
@@ -1,51 +1,98 @@
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TextOutputter.h>
+
 #include "RPN.hpp"
-#include <stdexcept>
-#include <gtest/gtest.h>
 
-// Test cases for correct return values
-TEST(RPNTest, CorrectReturnValues) {
-	RPN rpn;
-	EXPECT_EQ(5, rpn.calc("3 2 +"));
-	EXPECT_EQ(2, rpn.calc("5 3 -"));
-	EXPECT_EQ(12, rpn.calc("3 4 *"));
-	EXPECT_EQ(4, rpn.calc("8 2 /"));
-	EXPECT_EQ(26, rpn.calc("5 3 + 4 *"));
-	EXPECT_EQ(5, rpn.calc("7 4 2 * -"));
-	EXPECT_EQ(43, rpn.calc("3 5 8 * 7 + *"));
-}
+class TestRPN : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(TestRPN);
+    CPPUNIT_TEST(testAddition);
+    CPPUNIT_TEST(testSubtraction);
+    CPPUNIT_TEST(testMultiplication);
+    CPPUNIT_TEST(testDivision);
+    CPPUNIT_TEST(testErrorOnEmptyExpression);
+    CPPUNIT_TEST(testErrorOnInvalidExpression);
+    CPPUNIT_TEST(testErrorOnDivisionByZero);
+    CPPUNIT_TEST(testErrorOnOverflowInAddition);
+    CPPUNIT_TEST(testErrorOnOverflowInSubtraction);
+    CPPUNIT_TEST(testErrorOnOverflowInMultiplication);
+    CPPUNIT_TEST(testErrorOnOverflowInDivision);
+    CPPUNIT_TEST_SUITE_END();
 
-// Test cases for exceptions on invalid input
-TEST(RPNTest, InvalidInputExceptions) {
-	RPN rpn;
-	EXPECT_THROW(rpn.calc("3 & 2"), std::runtime_error);
-	EXPECT_THROW(rpn.calc("3 2"), std::runtime_error);
-	EXPECT_THROW(rpn.calc("3 2 + 3"), std::runtime_error);
-}
+public:
+	RPN	rpn;
 
-// Test cases for overflow exceptions
-TEST(RPNTest, OverflowExceptions) {
-	RPN rpn;
-	EXPECT_NO_THROW(rpn.calc("2147483647 0 +"));
-	EXPECT_THROW(rpn.calc("2147483647 1 +"), std::overflow_error);
-	EXPECT_NO_THROW(rpn.calc("-2147483648 0 +"));
-	EXPECT_THROW(rpn.calc("-2147483648 -1 +"), std::overflow_error);
+    void testAddition() {
+        CPPUNIT_ASSERT_EQUAL(7, rpn.calc("3 4 +"));
+		CPPUNIT_ASSERT_EQUAL(9, rpn.calc("0 9 +"));
+    }
 
-	EXPECT_NO_THROW(rpn.calc("2147483647 -2147483648 -"));
-	EXPECT_THROW(rpn.calc("2147483647 -1 -"), std::overflow_error);
-	EXPECT_NO_THROW(rpn.calc("-2147483648 2147483647 -"));
-	EXPECT_THROW(rpn.calc("-2147483648 1 -"), std::overflow_error);
+    void testSubtraction() {
+		CPPUNIT_ASSERT_EQUAL(-1, rpn.calc("3 4 -"));
+		CPPUNIT_ASSERT_EQUAL(-9, rpn.calc("0 9 -"));
+    }
 
-	EXPECT_NO_THROW(rpn.calc("46340 46340 *"));
-	EXPECT_THROW(rpn.calc("46341 46341 *"), std::overflow_error);
-	EXPECT_NO_THROW(rpn.calc("-46340 -46340 *"));
-	EXPECT_THROW(rpn.calc("-46341 -46341 *"), std::overflow_error);
+    void testMultiplication() {
+		CPPUNIT_ASSERT_EQUAL(12, rpn.calc("3 4 *"));
+		CPPUNIT_ASSERT_EQUAL(0, rpn.calc("0 9 *"));
+    }
 
-	EXPECT_NO_THROW(rpn.calc("-2147483648 -1 /"));
-	EXPECT_THROW(rpn.calc("-2147483648 -1 *"), std::overflow_error);
-	EXPECT_THROW(rpn.calc("-2147483648 0 /"), std::runtime_error);
-}
+    void testDivision() {
+		CPPUNIT_ASSERT_EQUAL(0, rpn.calc("3 4 /"));
+		CPPUNIT_ASSERT_EQUAL(0, rpn.calc("0 9 /"));
+		CPPUNIT_ASSERT_EQUAL(3, rpn.calc("6 2 /"));
+		CPPUNIT_ASSERT_EQUAL(3, rpn.calc("7 2 /"));
+    }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    void testErrorOnEmptyExpression() {
+        CPPUNIT_ASSERT_THROW(rpn.calc(""), std::runtime_error);
+    }
+
+    void testErrorOnInvalidExpression() {
+        CPPUNIT_ASSERT_THROW(rpn.calc("1 +"), std::runtime_error);
+		CPPUNIT_ASSERT_THROW(rpn.calc("1 + "), std::runtime_error);
+		CPPUNIT_ASSERT_THROW(rpn.calc("1 1 + "), std::runtime_error);
+        CPPUNIT_ASSERT_THROW(rpn.calc("+ 1"), std::runtime_error);
+        CPPUNIT_ASSERT_THROW(rpn.calc("1 a +"), std::runtime_error);
+    }
+
+    void testErrorOnDivisionByZero() {
+        CPPUNIT_ASSERT_THROW(rpn.calc("1 0 /"), std::runtime_error);
+    }
+
+    void testErrorOnOverflowInAddition() {
+        CPPUNIT_ASSERT_EQUAL(INT_MAX, rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 1 + 0 1 - *"));
+		CPPUNIT_ASSERT_THROW(rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 1 + 0 1 - * 1 +"), std::overflow_error);
+    }
+
+    void testErrorOnOverflowInSubtraction() {
+		CPPUNIT_ASSERT_EQUAL(INT_MAX, rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 1 + 0 1 - *"));
+        CPPUNIT_ASSERT_EQUAL(INT_MIN, rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"));
+		CPPUNIT_ASSERT_THROW(rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 1 -"), std::overflow_error);
+    }
+
+    void testErrorOnOverflowInMultiplication() {
+		CPPUNIT_ASSERT_EQUAL(INT_MAX, rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 1 + 0 1 - *"));
+		CPPUNIT_ASSERT_THROW(rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 1 + 0 1 - * 2 *"), std::overflow_error);
+        CPPUNIT_ASSERT_EQUAL(INT_MIN, rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"));
+		CPPUNIT_ASSERT_THROW(rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 2 *"), std::overflow_error);
+    }
+
+    void testErrorOnOverflowInDivision() {
+		CPPUNIT_ASSERT_EQUAL(INT_MIN, rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"));
+		CPPUNIT_ASSERT_THROW(rpn.calc("0 2 - 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 0 1 - /"), std::overflow_error);
+    }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestRPN);
+
+int main() {
+    CppUnit::Test* suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+    CppUnit::TextUi::TestRunner runner;
+    runner.addTest(suite);
+    bool wasSuccessful = runner.run("", false);
+    CppUnit::TextOutputter outputter(&runner.result(), std::cerr);
+    outputter.write();
+    return wasSuccessful ? 0 : 1;
 }
